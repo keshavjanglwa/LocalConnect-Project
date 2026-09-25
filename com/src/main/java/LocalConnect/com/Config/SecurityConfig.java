55eb43b71fae97a593cf6ef89302480fb334319b
keshavjanglwa <keshavjanglwa@gmail.com>
2026-09-25 14:22:02 +0530

    .

--- com/src/main/java/LocalConnect/com/Config/SecurityConfig.java
+++ com/src/main/java/LocalConnect/com/Config/SecurityConfig.java
@@ -21,11 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                // Public pages
                 .requestMatchers("/", "/login", "/register", "/admin/**" , "/css/**", "/js/**", "/images/**").permitAll()
-                // Admin-only pages
-                // .requestMatchers("/admin/**").hasRole("ADMIN")
-                // Everything else requires login
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
